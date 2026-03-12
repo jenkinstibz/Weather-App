a74a0a37d59716bc2e7c83523272cdf5904d22ee
jenkinstibz <jenkinstibenda@gmail.com>
2026-03-12 14:00:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from weather import main as get_weather
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenve("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
